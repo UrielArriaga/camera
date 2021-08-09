f14005eba831e5df6d52a428cdf170d677839e71
La Gran Ferretera <71093649+SistemasMB@users.noreply.github.com>
2021-08-09 13:40:14 -0500

    camera

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,68 @@
+import React from "react";
+import Webcam from "react-webcam";
 
-import React, {useRef} from "react";
+const Home = () => {
+  const webcamRef = React.useRef(null);
+  const mediaRecorderRef = React.useRef(null);
+  const [capturing, setCapturing] = React.useState(false);
+  const [recordedChunks, setRecordedChunks] = React.useState([]);
 
-
-export default function Home() {
-  const mediaRecorderRef = useRef(null);
-  const video = useRef(null);
-
-  const verify = () => {
-    navigator.mediaDevices.getUserMedia({video: true, audio: true}).then(
-      stream => {
-        console.log("here");
-      },
-      err => {
-        alert(
-          "Se ha denegado los permisos para acceder a la camara, para poder postularte permite a cvjobs.com.mx acceder"
-        );
-        console.error(err);
-      }
+  const handleStartCaptureClick = React.useCallback(() => {
+    setCapturing(true);
+    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
+      mimeType: "video/webm"
+    });
+    mediaRecorderRef.current.addEventListener(
+      "dataavailable",
+      handleDataAvailable
     );
+    mediaRecorderRef.current.start();
+  }, [webcamRef, setCapturing, mediaRecorderRef]);
 
-    if ("mediaDevices" in navigator && "getUserMedia" in navigator.mediaDevices) {
-      console.log("Let's get this party started");
-    } else {
-      console.log("Adasdas");
-    }
-  };
-  return (
-    <div>
-      <h1>Verify Camera</h1>
-
-      <video useRef={video} />
-      <button onClick={() => verify()}> Check</button>
-    </div>
+  const handleDataAvailable = React.useCallback(
+    ({ data }) => {
+      if (data.size > 0) {
+        setRecordedChunks((prev) => prev.concat(data));
+      }
+    },
+    [setRecordedChunks]
   );
-}
+
+  const handleStopCaptureClick = React.useCallback(() => {
+    mediaRecorderRef.current.stop();
+    setCapturing(false);
+  }, [mediaRecorderRef, webcamRef, setCapturing]);
+
+  const handleDownload = React.useCallback(() => {
+    if (recordedChunks.length) {
+      const blob = new Blob(recordedChunks, {
+        type: "video/webm"
+      });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      document.body.appendChild(a);
+      a.style = "display: none";
+      a.href = url;
+      a.download = "react-webcam-stream-capture.webm";
+      a.click();
+      window.URL.revokeObjectURL(url);
+      setRecordedChunks([]);
+    }
+  }, [recordedChunks]);
+
+  return (
+    <>
+      <Webcam audio={false} ref={webcamRef} />
+      {capturing ? (
+        <button onClick={handleStopCaptureClick}>Stop Capture</button>
+      ) : (
+        <button onClick={handleStartCaptureClick}>Start Capture</button>
+      )}
+      {recordedChunks.length > 0 && (
+        <button onClick={handleDownload}>Download</button>
+      )}
+    </>
+  );
+};
+
+export default Home;
